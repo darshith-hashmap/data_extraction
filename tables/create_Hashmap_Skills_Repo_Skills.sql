53cd2127254162a1903c949a501f891a3eb4b0fe
Srikanth Lakumarapu <srikanth.lakumarapu@hashmapinc.com>
2021-10-23 18:53:16 +0530

    Create create_Hashmap_Skills_Repo_Skills.sql

---
+++ tables/create_Hashmap_Skills_Repo_Skills.sql
@@ -0,0 +1,9 @@
+create or replace table srikant.Hashmap_Skills_Repo_Skills (
+Active string,
+Consultant_Name string,
+Skills_Bucket string,
+Skill_Sets string,
+Certifications_Completed string,
+Technology_Known string,
+Current_Active_Project string
+);
